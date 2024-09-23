40ca1c796b3aa7bbcd543555e8a0a16c77e57489
Simon Mathis <smathis@uw.edu>
2024-09-22 20:10:55 -0700

    feat: add launch script for pymol

---
+++ README.md
@@ -0,0 +1,9 @@
+# PyMOL RPC
+> A simple RPC client for sending commands and data between Python and PyMOL.
+
+## Credit
+This implementation is entirely based on the original [RDKit RPC](https://github.com/rdkit/rdkit/blob/master/rdkit/python/rdkit/Chem/PyMol.py) implementation and [PyMOL RPC] by Greg Landrum. Thank you Greg!
+
+## Installation
+
+## Basic idea
---
+++ src/pymolrpc/__init__.py
@@ -0,0 +1,36 @@
+def launch_pymol_with_rpc(args=None, block_input_hook=0):
+    """Launch PyMOL with RPC server (and any other pymol args)
+
+    Launch function taken directly from the open source pymol:
+    - https://github.com/schrodinger/pymol-open-source/blob/master/pyproject.toml#L42
+    - https://github.com/schrodinger/pymol-open-source/blob/master/modules/pymol/__init__.py#L405-L429
+
+    """
+    import sys
+
+    import pymol
+    from pymol import _cmd, _launch_no_gui, invocation, invocations, prime_pymol
+
+    if args is None:
+        args = sys.argv
+    invocation.parse_args(args)
+    invocations.deferred.append(
+        "_do__ /import pymol_rpc.server;pymol_rpc.server.launch_server()"
+    )
+
+    if invocation.options.gui == "pmg_qt":
+        if invocation.options.no_gui:
+            return _launch_no_gui()
+        elif invocation.options.testing:
+            return pymol._cmd.test2()
+
+        try:
+            from pmg_qt import pymol_qt_gui
+
+            return pymol_qt_gui.execapp()
+        except ImportError as ex:
+            print(f"Qt not available ({ex}), using GLUT/Tk interface")
+            invocation.options.gui = "pmg_tk"
+
+    prime_pymol()
+    _cmd.runpymol(None, block_input_hook)
